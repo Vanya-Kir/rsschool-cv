89333007361dafdbbcfe4edfbcac08595cacd448
Ivan Kirillov <van-kirillov@yandex.ru>
2021-09-19 15:29:56 +0300

    fix: fix menu

--- js/app.js
+++ js/app.js
@@ -1,13 +1,15 @@
 const tabs = document.querySelector('.tabs');
-const closeMenu = document.querySelector('.close_menu');
-const openMenu = document.querySelector('.open_menu');
+const closeMenu = document.querySelector('.close-menu');
+const openMenu = document.querySelector('.open-menu');
 
 openMenu.addEventListener('click', show);
 closeMenu.addEventListener('click', close);
 
 function show() {
+  // closeMenu.style.opacity = "1";
 	tabs.style.display = 'flex';
 	tabs.style.top = '0';
+  document.querySelector('.close-menu').style.display = "block";
 }
 function close() {
 	tabs.style.top = '-100%';
